fc7b9852f479a78bf5585da570f3639ba6eb79ac
Jonatas <jonatasgb@gmail.com>
2018-10-02 23:48:58 -0300

    Add files via upload

---
+++ script_mpd_ncmpcpp_dunst.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# instalação mpd
+sudo emerge -aq media-sound/mpd
+
+# instalação ncmpcpp
+sudo emerge -aq media-sound/ncmpcpp
+
+git clone https://github.com/quebravel/dotfiles-conf.git
+
+cd ~/dotfiles-conf/.config
+
+cp -rv dunst ~/.config
+
+cd ~/dotfiles-conf/
+
+cp -rv .mpd .mcmpcpp ~/
+
+# instalação mpc
+sudo emerge media-sound/mpc
+
+# instalação dunst
+sudo emerge x11-misc/dunst
+
+# adicionando ao iniciar
+sudo rc-service mpd start
+
+sudo rc-update add mpd default
+
+echo 'dunst &' >> ~/.xinitrc
+
+echo 'Pronto!'
