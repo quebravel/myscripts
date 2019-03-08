a6d27274e8f99f59ee099ca42e3f81ac1c3dd179
Jonatas <jonatasgb@gmail.com>
2019-03-08 00:47:14 +0000

    Update script_zsh.sh

--- script_zsh.sh
+++ script_zsh.sh
@@ -6,8 +6,6 @@ echo -e "\033[41;1;37m>>>\033[0m instalando/configurando zsh/oh-my-zsh"
 
 $t1
 
-su root
-
 emerge -auDN @world
 
 $t1
@@ -28,24 +26,20 @@ chsh -s /bin/zsh
 
 $t1
 
-su jonatas
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 $t1
 
-su root
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 $t1
 
-su jonatas
 
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/g' ~/.zshrc
 
 $t1
 
-su root
 
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="nanotech"/g' /root/.zshrc
 
