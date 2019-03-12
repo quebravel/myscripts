8c7e05dcf81774df37588f50f470d1cfe66bd91c
quebravel <jonatasgb@gmail.com>
2019-03-11 23:14:19 -0300

    mudei startx para xinit ... ficou mais rapido

--- script_auto_startx.sh
+++ script_auto_startx.sh
@@ -21,7 +21,7 @@ $t1
 echo '# Auto startx depending on the tty
 if [[ -z $DISPLAY ]] && (( $EUID != 0 )) {
     [[ ${TTY/tty} != $TTY ]] && (( ${TTY:8:1} <= 3 )) &&
-        startx 1>~/.log/xsession-errors 2>&1 &
+        xinit 1>~/.log/xsession-errors 2>&1 &
 }
 ' >> ~/.zprofile
 
