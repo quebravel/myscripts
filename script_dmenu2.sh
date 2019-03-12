d85ef08ddbdec536dda0e9155f2f6b78c8c222a4
quebravel <jonatasgb@gmail.com>
2019-03-12 19:04:09 -0300

    srcipt para instalar dmenu2

---
+++ script_dmenu2.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+echo 'DOWNLOAD DMENU2'
+
+if ! wget https://bitbucket.org/melek/dmenu2/downloads/dmenu2-0.2.tar.gz ~/
+then
+echo 'ERRO não foi possivel baixar dmenu2 verifique sua internet.'
+exit 1
+fi
+echo -e '\033[01;34mDmenu2 baixado!\033[0m'
+
+sleep 1
+
+cd ~/
+
+echo 'DESCOMPACTANDO PACOTE'
+
+tar -zxf dmenu2-0.2.tar.gz
+
+sleep 1
+
+cd dmenu2-0.2
+
+sleep 1
+
+echo 'COMPILANDO DMENU2'
+
+sudo make clean install
+
+echo -e '\033[01;34mDmenu2 copilado/instalando!\033[0m'
+
+sleep 1
+
+cd ~/
+
+sleep 1
+
+echo 'REMOVENDO PASTA'
+
+rm -r dmenu2-0.2 dmenu2-0.2.tar.gz
+
+echo -e '\033[01;34mPasta do dmenu2 removida!\033[0m'
+
+echo 'FIM DMENU2' 
