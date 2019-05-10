a4a14c17bbd69141729ca8f80718c5db678ddea5
Jonatas <jonatasgb@gmail.com>
2019-05-10 20:15:10 -0300

    Update bdInstall.sh

--- bdInstall.sh
+++ bdInstall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 sleep 2
 
@@ -7,10 +7,12 @@ sudo emerge media-libs/libsdl
 sleep 1
 
 mkdir ~/.zandronum/
+
 cd ~/.zandronum/
+
 sleep 1
 
-wget https://zandronum.com/downloads/zandronum3.0-linux-x86_64.tar.bz2
+wget -P ~/.zandronum https://zandronum.com/downloads/zandronum3.0-linux-x86_64.tar.bz2
 
 sleep 1
 
@@ -22,37 +24,23 @@ rm zandronum3.0-linux-x86_64.tar.bz2
 
 sleep 1
 
-wget https://github.com/dgeng/iwad/blob/master/doom2.7z?raw=true
+wget -P ~/.zandronum/ https://github.com/dgeng/iwad/blob/master/doom2.7z?raw=true
 
 sleep 1
 
-mv doom2.7z\?ram=true doom2.7z
+7z x doom2.7z*
 
 sleep 1
 
-7z x doom2.7z
+wget -P ~/.zandronum http://static.exciter.allfearthesentinel.net/wads/bd21test-jan02.pk3
 
 sleep 1
 
-rm doom2.7z
+#wget -P ~/.zandronum/DOOM_Music.rar https://www.moddb.com/downloads/mirror/119007/102/e65c50bbdc9fb47a1c329d666f23081e/?referer=https%3A%2F%2Fwww.moddb.com%2Fmods%2Fdoom-custom-music-pack-by-fix-03er
 
 sleep 1
 
-wget http://download1517.mediafire.com/tuc37swp3iwg/1a4cpb3px0a2b1j/bd21test-jan02.zip
-
-sleep 1
-
-unzip bd21test-jan02.zip
-
-sleep 1
-
-wget http://www.moddb.com/downloads/mirror/61238/102/54cb0dd4b64ac3965edd7653248ad0c8/?referer=http%3A%2F%2Fwww.moddb.com%2Fmods%2Fbrutal-doom%2Fdownloads
-
-mv index* sound.zip
-
-sleep 1
-
-unzip sound.zip
+#unzip DOOM_Music.rar
 
 echo concluido
 
