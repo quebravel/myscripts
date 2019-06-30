18a34fb607e1097409e96f7dc30b2d0a0d55deaf
Jonatas <jonatasgb@gmail.com>
2019-06-30 14:20:00 -0300

    Update dmen.sh

--- dmen.sh
+++ dmen.sh
@@ -3,4 +3,4 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-~/Downloads/dmenu2-0.2/dmenu_run -h 17 -y 22 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn "ohsnap" -p "dmenu2"
+~/Downloads/dmenu2-0.2/dmenu_run -h 17 -y 22 -nb "$color0" -nf "$color4" -sb "$color4" -sf "$color0" -fn "ohsnap" -p "dmenu2"
