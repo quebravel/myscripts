11e883d5cccd38596998c036ccfabe9d9772f786
Jonatas <jonatasgb@gmail.com>
2019-06-30 13:29:17 -0300

    Add files via upload

---
+++ dmen.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+
+# Import the colors
+. "${HOME}/.cache/wal/colors.sh"
+
+~/Downloads/dmenu2-0.2/dmenu_run -h 17 -y 22 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn "ohsnap" -p "dmenu2"
