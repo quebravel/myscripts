8a0970300158e9c3346a2a39f99fc5ffd7360c1b
Jonatas <jonatasgb@gmail.com>
2019-08-02 12:06:27 -0300

    Add files via upload

---
+++ mounta.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+dispositivo=`lsblk | tail -1 | cut -b 7-10`
+
+#variavel valor
+
+
+t1='sleep 0.5'
+
+opcao=$1
+
+mounta(){
+    sudo mount -t auto /dev/$dispositivo /mnt
+}
+
+Desmounta(){
+    sudo umount -R /mnt
+}
+
+escolha(){
+echo $opcao
+case $opcao in
+   "-m") mounta ;;
+   "--mount") mounta ;;
+   "-d") Desmounta ;;
+   "--desmount") Desmounta ;;
+   *) echo "Opção inválida! Use uma das seguintes opcoes:
+       -m,--mount                  Monta dispositivo.
+       -d,--desmount               Desmounta dispositivo."
+      exit 1 ;;
+esac
+}
+
+escolha
