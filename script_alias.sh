303fd73f41d31081a2877c5c03938572f9a95a6a
Jonatas <jonatasgb@gmail.com>
2019-08-02 17:58:31 -0300

    Add files via upload

---
+++ script_alias.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo '# xbsp
+alias xi="sudo xbps-install -S"
+alias xr="sudo xbps-remove -R"
+alias xo="sudo xbps-remove -Oo"
+alias xq="xbps-query -Rs"
+
+# default programs terminal
+export EDITOR="vim"
+export TERMINAL="urxvt"
+export BROWSER="firefox"
+export READER="zathura"
+alias n="nvim"
+alias v="vim"
+
+# power | /etc/sudoers | %<usuário> <hostname> =NOPASSWD: /usr/bin/shutdown
+alias shutdown -h now="sudo shutdown -h now"
+alias shutdown -r now="sudo shutdown -r now"' >> $HOME/.zshrc
