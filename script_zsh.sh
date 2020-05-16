8503e25ff6b2a0440feb0ad29df88b1e098f70ab
quebravel <jonatasgb@gmail.com>
2020-05-16 19:39:08 -0300

    add zshrc fshow

--- script_zsh.sh
+++ script_zsh.sh
@@ -132,7 +132,7 @@ na() {
 }
 # fh - repeat history
 fh() {
-  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -r 's/ *[0-9]*\*? *//' | sed -r 's/\\/\\\\/g')
+  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf -e +s --tac | sed -r 's/ *[0-9]*\*? *//' | sed -r 's/\\/\\\\/g')
 }
 # fkill - kill process
 fkill() {
@@ -143,6 +143,17 @@ fkill() {
   then
     echo $pid | xargs kill -${1:-9}
   fi
+}
+# fshow - git commit browser
+fshow() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
 }
 
 
