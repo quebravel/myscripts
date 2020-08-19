266422a1fa4a6228545a642c171e1904480d818e
quebravel <jonatasgb@gmail.com>
2020-08-18 23:03:26 -0300

    alias

--- script_zsh.sh
+++ script_zsh.sh
@@ -110,13 +110,51 @@ alias xo="sudo xbps-remove -Oo"
 
 alias ip="ip -s -c -h a"
 
-# lsd
-alias ls='lsd -tS --icon never'
-alias l='lsd -ltS --icon never'
-alias la='lsd -atS --icon never'
-alias lla='lsd -lSah --icon never'
-alias lt='lsd --tree --icon never'
-alias ll='lsd -lS --icon never --total-size'
+#DT
+### ARCHIVE EXTRACTION
+# usage: ex <file>
+ex ()
+{
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xjf $1   ;;
+      *.tar.gz)    tar xzf $1   ;;
+      *.bz2)       bunzip2 $1   ;;
+      *.rar)       unrar x $1   ;;
+      *.gz)        gunzip $1    ;;
+      *.tar)       tar xf $1    ;;
+      *.tbz2)      tar xjf $1   ;;
+      *.tgz)       tar xzf $1   ;;
+      *.zip)       unzip $1     ;;
+      *.Z)         uncompress $1;;
+      *.7z)        7z x $1      ;;
+      *.deb)       ar x $1      ;;
+      *.tar.xz)    tar xf $1    ;;
+      *.tar.zst)   unzstd $1    ;;      
+      *)           echo "'$1' cannot be extracted via ex()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
+
+# Changing "ls" to "exa"
+alias ls='exa -al --color-scale --color=always --group-directories-first' # my preferred listing
+alias la='exa -a --color-scale --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color-scale --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color-scale --color=always --group-directories-first' # tree listing
+alias l.='exa -a --color-scale | egrep "^\."'
+
+# youtube-dl
+alias yta-aac="youtube-dl --extract-audio --audio-format aac "
+alias yta-best="youtube-dl --extract-audio --audio-format best "
+alias yta-flac="youtube-dl --extract-audio --audio-format flac "
+alias yta-m4a="youtube-dl --extract-audio --audio-format m4a "
+alias yta-mp3="youtube-dl --extract-audio --audio-format mp3 "
+alias yta-opus="youtube-dl --extract-audio --audio-format opus "
+alias yta-vorbis="youtube-dl --extract-audio --audio-format vorbis "
+alias yta-wav="youtube-dl --extract-audio --audio-format wav "
+alias ytv-best="youtube-dl -f bestvideo+bestaudio "
 
 #fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
