7fd6d685f27c084b716da7fcccf88aab97fdb963
quebravel <jonatasgb@gmail.com>
2020-09-16 21:10:20 -0300

    add error alert

--- spy.sh
+++ spy.sh
@@ -6,25 +6,36 @@ usage(){
     -h              Chama essa ajuda."
 }
 
+spy(){
+
+    wget -q https://www.instagram.com/$nikename/ -P ~/
+
+    seguindo=`grep -oE '.{0,1}edge_follow".{0,14}' ~/index.html | sed 's/"edge_follow":{"count"://g' | sed 's/}//g'`
+
+    seguidores=`grep -oE '.{0,1}edge_followed_by".{0,14}' ~/index.html | sed 's/"edge_followed_by":{"count"://g' | sed 's/}//g'`
+
+    echo "Seguindo: $seguindo", Seguidores: $seguidores.
+
+    rm ~/index.html
+}
+
+
 while getopts ":n:h" o; do
     case "${o}" in
         n) nikename=$OPTARG
             ;;
-        h|?) usage
+        h) usage
             ;;
     esac
 done
 
-wget -q https://www.instagram.com/$nikename/ -P ~/
+# --- SE NENHUM ARGUMENTO --
 
-#<span class="g47SY " title="103">103</span>
-# "edge_followed_by":{"count":
-# "edge_follow":{"count":
+if [ -z "$nikename" ]
+then
+    usage
+else
+    spy
+fi
 
-seguindo=`grep -oE '.{0,1}edge_follow".{0,14}' ~/index.html | sed 's/"edge_follow":{"count"://g' | sed 's/}//g'`
 
-seguidores=`grep -oE '.{0,1}edge_followed_by".{0,14}' ~/index.html | sed 's/"edge_followed_by":{"count"://g' | sed 's/}//g'`
-
-echo "Seguindo: $seguindo", Seguidores: $seguidores.
-
-rm ~/index.html
