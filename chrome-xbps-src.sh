ae8bcda1d8f63afd7ec668b44341224c107ad5eb
quebravel <jonatasgb@gmail.com>
2021-03-19 21:32:48 -0300

    update

--- chrome-xbps-src.sh
+++ chrome-xbps-src.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-cd ~/void-packages
+dvpack="$HOME/void-packages"
+
+if [ -e $dvpack ]; then
+    cd $dvpack
+else
+    git clone https://github.com/void-linux/void-packages ; cd $dvpack
+fi
+
+pwd
+
+./xbps-src binary-bootstrap
+
+sleep 2
+
+echo XBPS_ALLOW_RESTRICTED=yes >> ~/void-packages/etc/conf
+
+sleep 0.5
 
 git pull origin master
 
+sleep 1
+
 ./xbps-src pkg google-chrome
 
-sudo xbps-install -u --repository=/home/jonatas/void-packages/hostdir/binpkgs/nonfree google-chrome
+sudo xbps-install -u --repository=$dvpack/hostdir/binpkgs/nonfree google-chrome
 
 
