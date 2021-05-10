d5aa06a6f8c75d6f783c6f55bdb0f1bdc28c7413
Quebravel <jonatasgb@gmail.com>
2021-05-10 15:15:23 -0300

    batch para iniciar MAXdata, skype, catalogoBastos, anydesk e whatsapps.

---
+++ iniciarMax/iniciar.bat
@@ -0,0 +1,19 @@
+timeout /t 240
+
+start C:\MAX\MAX_Manager2.exe
+
+timeout /t 60
+
+start skype.exe
+
+timeout /t 40
+
+start AnyDesk.exe
+
+timeout /t 50
+
+start C:\Users\Jonatas\AppData\Local\WhatsApp\WhatsApp.exe
+
+timeout /t 20
+
+start C:\Users\Jonatas\"WebCatalog Apps"\WhatsApp\WhatsApp.exe
---
+++ iniciarMax/initing.vbs
@@ -0,0 +1,5 @@
+' mover este aquivo para C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
+set objSh = CreateObject("WScript.Shell")
+
+' caminho para script
+objSh.Run "cmd /k C:\Users\Jonatas\Desktop\iniciarMax\iniciar.bat", 0
