1100d19d1405dba65e95f090cc7312e264962acf
quebravel <jonatasgb@gmail.com>
2021-06-13 18:42:49 -0300

    add scrip configuration

---
+++ scrip_configALL.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# quando o pulseaudio nao esta iniciando, desabilitar economia de energia.
+sudo echo "options snd-hda-intel power_save=0 power_save_controller=N" > /etc/modprobe.d/alsa-base.conf
+
+# comfigurar tema do mouse.
+mkdir -p ~/.icons/default
+echo -e "[icon theme]\nInherits=Vanilla-DMZ-AA" > ~/.icons/default/index.theme
+
+mkdir -p ~/.config/gtk-3.0
+echo -e "[Settings]\ngtk-cursor-theme-name=Vanilla-DMZ-AA" > ~/.config/gtk-3.0/settings.ini
+
+# adicionar ao runit.
+sudo ln -s /etc/sv/dbus /var/service/
+sudo ln -s /etc/sv/pulseaudio /var/service/
+sudo ln -s /etc/sv/mpd /var/service/
+sudo ln -s /etc/sv/vnstatd /var/service/
+sudo ln -s /etc/sv/sshd /var/service/
+
+# tema gruvbox
+mkdir ~/.themes
+mkdir ~/.icons
+
+git clone https://github.com/jmattheis/gruvbox-dark-gtk ~/.themes/gruvbox-dark-gtk
+
+git clone https://github.com/jmattheis/gruvbox-dark-icons-gtk ~/.icons/gruvbox-dark-icons-gtk
