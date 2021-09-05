905ba7f39e605a12d90ac72233d7f5dcdfc2e2ed
quebravel <jonatasgb@gmail.com>
2021-09-05 15:21:11 -0300

    add fix3 zshalias

--- script_zsh_alias.sh
+++ script_zsh_alias.sh
@@ -3,7 +3,7 @@
 
 zsh_alias(){
 
-sudo xbps-install -Sy zsh zsh-{autosuggestions,completions}
+sudo xbps-install -Sy zsh zsh-autosuggestions zsh-completions
 #,syntax-highlighting
 echo "-> -> -> -> -> -> ESCREVA O NOME DO USUÁRIO <- <- <- <- <- <-"
 
