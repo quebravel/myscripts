357818234e95765c201c702d6d150a7ae88f4732
Jonatas <jonatasgb@gmail.com>
2021-11-22 00:52:20 -0300

    Update chdman-script.sh

--- chdman-script.sh
+++ chdman-script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# credit from site and user Clyde for scrit
+# credit from site and user Clyde for scritp
 # https://retropie.org.uk/forum/topic/25770/linux-script-to-batch-convert-psx-bin-cue-to-chd/4
 
+# first install mame-tools
 
 for i in *.cue; do chdman createcd -i "$i" -o "${i%.*}.chd"; done
