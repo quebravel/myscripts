d6d004e3cb3d57ef41631d0570fb8d249fbd2570
quebravel <jonatasgb@gmail.com>
2021-12-05 14:10:25 -0300

    add update

--- script_cp_recursive_extension.sh
+++ script_cp_recursive_extension.sh
@@ -6,3 +6,7 @@ mkdir target_directory \; cp **/*.xls target_directory
 # bash
 mkdir newDir \; find . -name \*.xls -exec cp {} /newDir \;
 
+# bash script
+find [SOURCEPATH] -type f -name '[PATTERN]' | 
+    while read P; do cp --parents "$P" [DEST]; done
+
