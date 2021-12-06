a97bbc18e5a8627987923c54dcb51d8b5ba71ddf
quebravel <jonatasgb@gmail.com>
2021-12-05 21:13:12 -0300

    update

--- chdman-script.sh
+++ chdman-script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# credit from site and user Clyde for scritp
+# credit from site and user Clyde for scrit
 # https://retropie.org.uk/forum/topic/25770/linux-script-to-batch-convert-psx-bin-cue-to-chd/4
 
-# first install mame-tools
 
-# obs: the format chd no rum in emulator ePSXe, only works in emulator duckstation
+for i in *.cue; do chdman createcd -i "$i" -o "${i%.*}.chd"; notify-send -u low "$i" "Terminado" ; done
 
-for i in *.cue; do chdman createcd -i "$i" -o "${i%.*}.chd"; done
+notify-send -t 10000 -u critical "chdman conversão" "Finalizada"
+
+# mover para a pasta
+#mv *.chd /home/jonatas/Downloads/ROMS_BT-BR/"PLAYSTATION PT-BR"/chdROONS/
