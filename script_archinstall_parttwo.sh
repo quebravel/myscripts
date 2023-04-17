bb8919482e9c9b7e7523dd6918bdcb97b3745f19
Jonatas <jonatasgb@gmail.com>
2023-04-17 19:44:58 -0300

    Update script_archinstall_parttwo.sh

--- script_archinstall_parttwo.sh
+++ script_archinstall_parttwo.sh
@@ -1,39 +1,58 @@
 #!/bin/bash
+
 _so="sudo pacman -S"
 _s="sudo"
 
+programas(){
 $_so xdo mtools xdotool exa maim mpv feh xclip xsel python-pynvim yt-dlp the_silver_searcher git wget ntfs-3g xorg-{xsetroot,xset,xrdb,fonts} xf86-input-{evdev,libinput} curl alsa-utils zathura-pdf-poppler adwaita-icon-theme bpytop xcursor-vanilla-dmz-aa base-devel nodejs go cmake libxinerama libxft python-pip sxiv alacritty xdg-user-dirs ffmpeg pulseaudio
+}
 
-
-xdg-user-dirs-update
-
+zshconfig(){
 $_so zsh zsh-completions 
+}
 
+fontes(){
 # fonts
 $_so ttf-dejavu
+}
 
+drive_video(){
 # drive video
 $_so xorg-{server,xinit} xf86-video-amdgpu amdvlk
+}
 
+navegador(){
 # navegador
 echo "Navegador"
 $_so qutebrowser
 echo "Adicionando dicionário"
 /usr/share/qutebrowser/scripts/dictcli.py install pt-BR
+}
 
+windownmanager(){
 # wm
 $_so bspwm sxhkd polybar
+}
 
+github_config(){
 # dotfiles
 git clone https://github.com/quebravel/dotfiles-conf ~/dotfiles-conf
 cp -r ~/dotfiles-conf/.config ~/
+}
 
+configuracoes(){
+xdg-user-dirs-update
 
+}
+
+temas(){
 # gruvbox tema
 mkdir -p ~/themes ~/icons
 git clone https://github.com/jmattheis/gruvbox-dark-gtk ~/.themes/gruvbox-dark-gtk
 git clone https://github.com/jmattheis/gruvbox-dark-icons-gtk ~/.icons/gruvbox-dark-icons-gtk
+}
 
+autoscript(){
 # AUTO SCRIPTS sh -c "$()"
 
 # ok ohmyzsh
@@ -47,3 +66,16 @@ git clone https://github.com/jmattheis/gruvbox-dark-icons-gtk ~/.icons/gruvbox-d
 
 # yay
  sh -c "$(https://raw.githubusercontent.com/quebravel/myscripts/master/script_yay.sh)"
+
+}
+
+programas
+zshconfig
+fontes
+drive_video
+navegador
+windownmanager
+github_config
+configuracoes
+temas
+autoscript
